a53f82b8729e60c5c5622756a8cb7c68eda7b2b5
Jason Tam <jasontamemail@gmail.com>
2020-02-17 00:46:34 -0500

    using a local library I created works, the library is call AppLogger

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,7 @@
+
+<
+build.gradle,f/0/f07866736216be0ee2aba49e392191aeae700a35
+l
+<src/com/jasontam/learnjava8features/appdriver/AppDriver.java,a/b/abe6f62571789c46c813a75afbc0aedafc3d49a9
+B
+logging.properties,9/c/9c837130f6efd9b07adfd55e135d5efc94e77667
---
+++ src/com/jasontam/learnjava8features/appdriver/AppDriver.java
@@ -0,0 +1,25 @@
+package com.jasontam.learnjava8features.appdriver;
+
+import com.jasontam.learngradlejavaproject.applogging.AppLogger;
+
+import java.util.logging.ConsoleHandler;
+import java.util.logging.Logger;
+
+public class AppDriver {
+
+    public static void main(String[] args) {
+
+        AppLogger.setLogger(AppDriver.class.getName());
+        Logger log = AppLogger.getInstance().getAppLogger();
+
+        log.addHandler(new ConsoleHandler());
+
+        log.warning("STARTING");
+
+        String thisMethodName = new Object() {}.getClass().getEnclosingMethod().getName();
+
+        log.entering(AppDriver.class.getName(), thisMethodName);
+        log.exiting(AppDriver.class.getName(), thisMethodName);
+
+    }
+}
